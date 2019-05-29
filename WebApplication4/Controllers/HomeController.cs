50ed27f1080837b5d256eefbe7606d8fa0c95f12
Chris Phillips <you@example.com>
2019-05-29 09:19:14 -0400

    Review 5

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Calculator()
+        {
+            return View();
+        }
+
+        public ActionResult Result(int num1, int num2, string operation)
+        {
+            switch (operation)
+            {
+                case "Plus": ViewBag.Result = num1 + num2;
+                    break;
+                case "Minus":
+                    ViewBag.Result = num1 - num2; 
+                    break;
+                case "Multiply":
+                    ViewBag.Result = num1 * num2;
+                    break;
+                case "Divide":
+                    ViewBag.Result = num1 / num2;
+                    break;
+            }
+            ViewBag.Operation = operation;   
+            return View();
+        }
+
+    }
+}
---
+++ WebApplication4/Models/Operation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class Operation
+    {
+        public double num1 { set; get; }
+        public double num2 { set; get; }
+
+        public Operation()
+        {
+
+        }
+    }
+}
